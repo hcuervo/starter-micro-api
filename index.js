01b1e07028caf8c732baea85a35d5497c60a4222
Horacio Cuervo <horaciocuervo@gmail.com>
2023-06-07 15:38:27 -0300

    index.js start

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var http = require('http');
+/*var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3000);*/
+
+const { createRuntime } = require('@node-red/runtime');
+
+async function startNodeRED() {
+  const runtime = await createRuntime();
+  await runtime.start();
+  console.log('Node-RED started!');
+}
+
+startNodeRED().catch((err) => {
+  console.error('Failed to start Node-RED', err);
+});
