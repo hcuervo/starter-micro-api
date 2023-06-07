91494ad3250c576fa4151df4c8de126b09625e7d
Horacio Cuervo <horaciocuervo@gmail.com>
2023-06-07 15:49:20 -0300

    index.js prueba 2

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-/*var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);*/
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
 
-const http = require('http');
-const { createRuntime } = require('@node-red/runtime');
+// Create an Express app
+var app = express();
 
-async function startNodeRED() {
-  const runtime = await createRuntime();
-  await runtime.start();
-  console.log('Node-RED started!');
-}
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
 
-const server = http.createServer(async (req, res) => {
-  if (req.url === '/admin') {
-    res.writeHead(302, {
-      'Location': 'http://localhost:1880',
-    });
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
-});
+// Create a server
+var server = http.createServer(app);
 
-server.listen(80, 'localhost', () => {
-  console.log('Node-RED server is running on port 80!');
-  startNodeRED().catch((err) => {
-    console.error('Failed to start Node-RED', err);
-  });
-});
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"/home/nol/.nodered/",
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(8000);
+
+// Start the runtime
+RED.start();
